4e74104eefaadce7adfc801ac1ef797b5bce8c9c
Maclavio <stecesar7uu7@gmail.com>
2023-10-17 15:09:32 -0600

    Se sube el progframa principal

---
+++ src/programa/pkg2/Programa2.java
@@ -0,0 +1,13 @@
+package programa.pkg2;
+
+import programa.pkg2.Personaje.AdivinaQuien;
+
+public class Programa2 {
+
+    public static void main(String[] args) {
+        AdivinaQuien a = new AdivinaQuien();
+        
+        a.setVisible(true);
+    }
+    
+}
